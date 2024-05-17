f6d479dfd98d790c14ab07c7b05df2c25f5f90b2
Gabriel Gontijo <gontijogabr@gmail.com>
2024-05-16 21:20:28 -0300

    feat: api v2 com viewsets, mixins e routers

--- cursos/models.py
+++ cursos/models.py
@@ -32,6 +32,7 @@ class Avaliacao(Base):
         verbose_name = 'Avaliação'
         verbose_name_plural = 'Avaliações'
         unique_together = ['email', 'curso']
+        ordering = ['id']
 
     def __str__(self):
         return f'{self.nome} avaliou o curso {self.curso} com nota: {self.avaliacao}'
--- cursos/serializers.py
+++ cursos/serializers.py
@@ -22,7 +22,16 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 
 
 class CursoSerializer(serializers.ModelSerializer):
+    # Nested Relationship, recomendado para OneToOneField
+    # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
 
+    # HyperLinked Related Field, recomendado para ManyToManyField
+    # avaliacoes = serializers.HyperlinkedIdentityField(many=True, 
+    #                                                   read_only=True, 
+    #                                                   view_name='avaliacao-detail')
+
+    # Primary Key Related Field, ideal para performance
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = Curso
         fields = (
@@ -31,4 +40,5 @@ class CursoSerializer(serializers.ModelSerializer):
             'url',
             'criacao',
             'ativo',
+            'avaliacoes'
         )
